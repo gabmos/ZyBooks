2b4aaa587afd6ebef2a18a02e272e46539fa2c76
Gabriel <gbgh8@umsystem.edu>
2021-02-11 17:34:13 -0300

    6.3

---
+++ User-defined function/6.3 Finding values in array/PrintingAnArrayElementsSeparetedByComma.cpp
@@ -0,0 +1,33 @@
+/*
+Write a for loop to print all NUM_VALS elements of array hourlyTemp. Separate elements with a comma and space. Ex: If hourlyTemp = {90, 92, 94, 95}, print:
+90, 92, 94, 95
+Your code's output should end with the last element, without a subsequent comma, space, or newline.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   const int NUM_VALS = 4;
+   int hourlyTemp[NUM_VALS];
+   int i;
+
+   for (i = 0; i < NUM_VALS; ++i) {
+      cin >> hourlyTemp[i];
+   }
+
+   /* Your solution goes here  */
+	for(i = 0; i < NUM_VALS; i++){
+		if(i < NUM_VALS-1){
+			cout << hourlyTemp[i] << ", ";
+		}
+		else{
+			cout << hourlyTemp[i];
+		}
+	}
+   
+
+   cout << endl;
+
+   return 0;
+}
